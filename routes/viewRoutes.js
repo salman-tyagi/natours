bd9c3a61e90c4084f55e8f74e9923889b61c7910
salman-tyagi <salmantyagi15@gmail.com>
2023-04-24 17:48:19 +0530

    Fixed object passed

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get(
   '/',
-  bookingController.createBookinCheckout,
+  // bookingController.createBookinCheckout,
   authController.isLoggedIn,
   viewController.getOverview
 );
